9d3019f62fa24186f898839310354e3efdc9c208
itzSumma <itzSumma11@gmail.com>
2026-02-04 22:24:56 +0600

    added the practise-2

---
+++ practise-2.js
@@ -0,0 +1,78 @@
+/**
+ * Problem 2: Student Marks Analyzer
+Function name: analyzeMarks(marksObj)
+Statement:
+ Return total marks, average marks, highest scoring subject, and lowest scoring subject.
+Test case 1
+Input: { math: 78, english: 65, physics: 88, bangla: 55 }
+
+Output: { total: 286, average: 71.5, highest: "physics", lowest: "bangla" }
+
+ */
+
+// function analyzeMarks(marksObj) {
+//   console.log(marksObj);
+//   let total = 0;
+//   let highestMarks = -Infinity;
+//   let lowestMarks = Infinity;
+//   for (let key in marksObj) {
+//     total += marksObj[key];
+// if (marksObj[key]> highestMarks){
+//     highestMarks = marksObj[key];
+//     highestSubject = key;
+// }
+// if (marksObj[key]< lowestMarks ){
+//     lowestMarks = marksObj[key];
+// lowestSubject = key
+
+// }
+//     // console.log(key, "->", marksObj[key]);
+//   }
+// //   console.log('highest--->', highestMarks)
+// //   console.log('lowest--->', lowestMarks)
+//   let avarage = total / Object.keys(marksObj).length;
+//   // console.log( "Total -> " ,total);
+//   // console.log( "Avarage-> " ,avarage);
+//   return {
+//     total,
+//     avarage,
+//     // highestMarks,
+//      highestSubject,
+//     // lowestMarks,
+//     lowestSubject
+//   };
+// }
+// let output = analyzeMarks({ math: 78, english: 65, physics: 88, bangla: 55 });
+// console.log(output);
+
+function analyzeMarks(marksObj) {
+    total =0;
+    let highestmark = -Infinity;
+    let lowestmark = Infinity;
+  for (let subject in marksObj) {
+    let mark = marksObj[subject];
+    total += mark;
+
+    if (mark > highestmark){
+        highestmark = mark;
+        highestSubject = subject; 
+    }
+    if (mark< lowestmark) {
+        lowestmark = mark;
+        lowestSubject = subject;
+        
+    }
+}
+let avarage = total / Object.keys(marksObj).length
+return {
+    total,
+    avarage,
+    highestmark,
+    lowestmark,
+    highestSubject,
+    lowestSubject
+
+}
+}
+let output = analyzeMarks({ math: 78, english: 65, physics: 88, bangla: 55 });
+console.log(output);
