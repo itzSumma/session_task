3efbe769540f46fbe621bb9a001a77faddd4c547
itzSumma <itzSumma11@gmail.com>
2026-02-05 09:43:25 +0600

    add the new function i

--- practise-2.js
+++ practise-2.js
@@ -45,34 +45,71 @@ Output: { total: 286, average: 71.5, highest: "physics", lowest: "bangla" }
 // let output = analyzeMarks({ math: 78, english: 65, physics: 88, bangla: 55 });
 // console.log(output);
 
-function analyzeMarks(marksObj) {
-    total =0;
-    let highestmark = -Infinity;
-    let lowestmark = Infinity;
-  for (let subject in marksObj) {
-    let mark = marksObj[subject];
-    total += mark;
+// function analyzeMarks(marksObj) {
+//     total =0;
+//     let highestmark = -Infinity;
+//     let lowestmark = Infinity;
+//   for (let subject in marksObj) {
+//     let mark = marksObj[subject];
+//     total += mark;
 
-    if (mark > highestmark){
-        highestmark = mark;
-        highestSubject = subject; 
-    }
-    if (mark< lowestmark) {
-        lowestmark = mark;
-        lowestSubject = subject;
-        
-    }
-}
-let avarage = total / Object.keys(marksObj).length
-return {
+//     if (mark > highestmark){
+//         highestmark = mark;
+//         highestSubject = subject;
+//     }
+//     if (mark< lowestmark) {
+//         lowestmark = mark;
+//         lowestSubject = subject;
+
+//     }
+// }
+// let avarage = total / Object.keys(marksObj).length
+// return {
+//     total,
+//     avarage,
+//     highestmark,
+//     lowestmark,
+//     highestSubject,
+//     lowestSubject
+
+// }
+// }
+// let output = analyzeMarks({ math: 78, english: 65, physics: 88, bangla: 55 });
+// console.log(output);
+
+/**
+ * 
+Test case 2
+Input: { ict: 90, biology: 90, chemistry: 70 }
+Output: { total: 250, average: 83.33, highest: "ict", lowest: "chemistry" }
+
+ */
+function analyzeMarks(marksObj) {
+  total = 0;
+highestmarks = -Infinity;
+lowestmarks = Infinity;
+  for (let subject in marksObj) {
+    total += marksObj[subject];
+    if (marksObj[subject] > highestmarks) {
+        highestmarks = marksObj[subject];
+        hightsubject = subject;
+            }
+            if (marksObj[subject]< lowestmarks) {
+                lowestmarks = marksObj[subject];
+                lowestsubject = subject;
+                
+            }
+  }
+  let avarage = total / Object.keys(marksObj).length;
+  avarage =Number(avarage.toFixed(2));
+  return {
     total,
     avarage,
-    highestmark,
-    lowestmark,
-    highestSubject,
-    lowestSubject
-
+    highestmarks,
+    hightsubject,
+    lowestmarks,
+    lowestsubject
+  };
 }
-}
-let output = analyzeMarks({ math: 78, english: 65, physics: 88, bangla: 55 });
+let output = analyzeMarks({ ict: 90, biology: 90, chemistry: 70 });
 console.log(output);
